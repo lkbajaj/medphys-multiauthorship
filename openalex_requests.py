20cb80505fc763a15ec2d02c1b6adcf3d82dc58b
lkbajaj <leynabajaj@umass.edu>
2025-07-10 22:29:28 -0400

    polite pool

--- openalex_requests.py
+++ openalex_requests.py
@@ -2,10 +2,17 @@ import requests
 import time
 import pandas as pd
 import numpy as np
+import json 
+
+with open('sensitive.json','r') as file:
+    data = json.load(file)
+    EMAIL = data['email']
+
 
 # cursor method to get all papers, recommended by ChatGPT
 def fetch_all_openalex_results(url,params,delay=0.5):
     params = params.copy()
+    params['mailto'] = EMAIL
     params['cursor'] = '*'
     params['per_page'] = params.get('per-page',200)
 
@@ -31,6 +38,7 @@ def get_citation_counts(work_id):
     response = requests.get(url)
     response.raise_for_status()
     result = response.json()
+    citations = result['cited_by_count']
     year = int(result['publication_year'])
 
     url = "https://api.openalex.org/works"
@@ -41,13 +49,10 @@ def get_citation_counts(work_id):
     response.raise_for_status()
 
     results = response.json().get("results", [])
-
-    citations = 0
     citations5yr = 0
     for work in fetch_all_openalex_results(url,params):
         pub_year = int(work.get("publication_year", None))
         if pub_year is not None:
-            citations+=1
             pub_year = int(pub_year) 
             if pub_year <= year + 5:
                 citations5yr+=1
@@ -58,8 +63,12 @@ def get_citation_counts(work_id):
 
 
 # get publications from the Physics in Medicine and Biology journal from the year 2000
-sourcedict = {'Physics in Medicine and Biology':'S20241394'}
-source_id  = sourcedict['Physics in Medicine and Biology']
+sourcedict = [
+    {'name':'Physics in Medicine and Biology','id':'S20241394','year_start':1956,'year_end':2019},
+    {'name':'Medical Physics','id':'S95522064'}
+              ]
+source_id  = sourcedict[0]['id']
+source_name = sourcedict[0]['name']
 
 YEAR_START=1956
 YEAR_END=2019
@@ -102,4 +111,4 @@ for year in years:
         print(rows[-1])
 
     df = pd.DataFrame(rows)
-    df.to_csv(f'spreadsheets/medphys-openalex{year}.csv',index=False)
+    df.to_csv(f'spreadsheets/{source_name}-{year}.csv',index=False)
